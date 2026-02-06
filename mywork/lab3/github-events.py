13326da3126e8bb127562cc816ab62641bae0507
borammin <77897938+borammin@users.noreply.github.com>
2026-02-06 09:26:49 -0500

    lab3-python

--- mywork/lab3/github-events.py
+++ mywork/lab3/github-events.py
@@ -2,14 +2,14 @@
 
 import os
 import json
-#import requests
+import requests
 
 GHUSER = os.getenv('GITHUB_USER')
 
 url = f'https://api.github.com/users/{GHUSER}/events'
 
-#r = json.loads(requests.get(url).text)
+r = json.loads(requests.get(url).text)
 
-#for x in r[:5]:
- # event = x['type'] + ' :: ' + x['repo']['name']
- # print(event)
+for x in r[:5]:
+    event = x['type'] + ' :: ' + x['repo']['name']
+    print(event)
