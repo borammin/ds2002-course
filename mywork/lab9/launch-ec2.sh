df6e9b012fea7935356c4b79d8dfa6dcc0b035a8
Boram Min <boramjuliemin@gmail.com>
2026-03-27 14:58:03 -0400

    completed lab9

---
+++ mywork/lab9/launch-ec2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI=ami-042e8287309f5df03
+INSTANCE_TYPE=t2.nano
+INSTANCE_NAME=ds2002-tsc9rv
+KEY_NAME=key-ec2
+SECURITY_GROUP_ID=sg-0be6679b34372f15a
+SUBNET_ID=subnet-085a38474f2e3b60c
+
+aws ec2 run-instances \
+  --image-id $AMI \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --security-group-ids $SECURITY_GROUP_ID \
+  --subnet-id $SUBNET_ID \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+  --output json
